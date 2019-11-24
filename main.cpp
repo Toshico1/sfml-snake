e706fa61e453c598b2318b7f6a7639371ed7adcf
Lex <43500461+Toshico1@users.noreply.github.com>
2019-11-25 02:03:53 +0300

    Добавил игровое поле

--- main.cpp
+++ main.cpp
@@ -1,24 +1,53 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace sf;
 
+ 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(600, 200), "Snake");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
+	// Объект, который, собственно, является главным окном приложения
+	RenderWindow window(VideoMode(600, 600), "SFML Works!");
+	
+	// Главный цикл приложения. Выполняется, пока открыто окно
+	while (window.isOpen())
+	{
+		// Обрабатываем очередь событий в цикле
+		Event event;
+		while (window.pollEvent(event))
+		{
+			// Пользователь нажал на «крестик» и хочет закрыть окно?
+			if (event.type == Event::Closed)
+				// Тогда закрываем его
+				window.close();
+		}
+	// Отрисовка окна
+        RectangleShape lines[100][2];	//[][0] - vert [][1] - hor
+        
+        //рисуем поле
+        for(int i=0; i < 100; i++){
+            RectangleShape line_v(Vector2f(600, 2)); 
+            RectangleShape line_h(Vector2f(600, 2));
+            line_v.setPosition(i*10, 0);
+            line_h.setPosition(0, i*10);
+            line_v.rotate(90);
+            //line_h.rotate(90); не нужно
+            line_v.setFillColor(Color(i+100, i, i+20));
+            line_h.setFillColor(Color(i+100, i, i+20));
+            lines[i][0] = line_v;
+            lines[i][1] = line_h;
         }
+        //далее для каждой из точек задаем свою позицию:
 
-        window.clear();
-        window.draw(shape);
+        for(int i=0; i < 100; i++){
+            window.draw(lines[i][0]);
+        }
+        for(int i=0; i<100; i++){
+            window.draw(lines[i][1]);
+        }
+        //и в конце выводим все на экран:
         window.display();
-    }
-
-    return 0;
+	}
+ 
+	return 0;
 }
